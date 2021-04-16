9e2b9f11ec4437ccdadfcefdd2ad73c14ab5d1b0
jakala <angel.cid@gmail.com>
2021-04-16 18:34:34 +0200

    instalado guzzle. preparamos un cliente http propio para separar infraestructura

---
+++ src/Infrastructure/Exception/ClientException.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Infrastructure\Exception;
+
+use \Exception;
+
+class ClientException extends Exception
+{
+    public function __construct(\Exception $e) {
+        parent::__construct();
+        $this->message = "Connection to punkApi error: ".$e->getMessage();
+        $this->code = 500;
+    }
+}
---
+++ src/Infrastructure/Service/HttpClient.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Infrastructure\Service;
+
+use App\Domain\Entity\Beer;
+use App\Domain\Entity\BeerList;
+use App\Domain\ValueObject\Food;
+use App\Infrastructure\Exception\ClientException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class HttpClient
+{
+    const URI = 'https://api.punkapi.com/v2';
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    public function searchByCriteria(Food $food)
+    {
+        $uri = self::ROOT_ENDPOINT.'/beers?'.http_build_query(['food' => $food->getFood()]);
+        try {
+            $beerList = new BeerList();
+            $results = $this->client->get($uri)->getBody()->getContents();
+            $results = json_decode($results, true);
+            foreach($results as $item) {
+                $beer = new Beer($item['id'], $item['name'], $item['description']);
+                $beerList->addBeer($beer);
+            }
+            return $beerList;
+        } catch (GuzzleException $e) {
+            throw new ClientException($e);
+        }
+
+    }
+}
